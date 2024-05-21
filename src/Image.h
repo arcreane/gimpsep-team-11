8fdc60045d1e94e952b68c4fa48aa5719b50bcf9
Sarenela <sarenela@op.pl>
2024-05-21 10:48:44 +0200

    change structure

---
+++ src/Image.h
@@ -0,0 +1,22 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <opencv2/core.hpp>
+#include <string>
+
+class Image {
+public:
+    Image();  // Default constructor
+    Image(const std::string& path);
+    Image(const cv::Mat& image, const std::string& path = "");
+
+    cv::Mat getImage() const;
+    std::string getPath() const;
+    void setImage(const cv::Mat& newImage);
+
+private:
+    cv::Mat image;
+    std::string path;
+};
+
+#endif // IMAGE_H
--- src/image_editor/ImageEditor.h
+++ src/image_editor/ImageEditor.h
@@ -1,35 +1,33 @@
 #ifndef IMAGEEDITOR_H
 #define IMAGEEDITOR_H
 
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
+#include "../Image.h"
+#include "../ImageManager.h"
 #include <string>
+#include <vector>
 
 class ImageEditor {
-private:
-    cv::Mat image;
-    bool hasValidExtension(const std::string& filename);
-    bool checkImageLoaded() const;
-
 public:
-    ImageEditor() {}
-    ImageEditor(const std::string& imagePath);
+    ImageEditor(const std::string& imagePath = "");
 
     void loadImage(const std::string& imagePath);
+    void loadMultipleImages(const std::vector<std::string>& imagePaths);
     bool saveImage(const std::string& outputPath);
     void displayImage() const;
-
     void dilateImage(int size);
     void erodeImage(int size);
     void resizeImage(double scale);
     void resizeImage(int width, int height);
     void lightenDarken(double factor);
-    void stitchImages(const std::vector<std::string>& filenames);
+    void stitchImages(const std::vector<int>& indices);
     void cannyEdgeDetection(double lowerThreshold, double upperThreshold, int kernelSize);
+    void undo();
+    void selectImage(int index);
+    std::vector<Image> getAllImages() const;
 
-    cv::Mat getImage() const;
-    virtual ~ImageEditor(){}
+private:
+    bool checkImageLoaded() const;
+    ImageManager imageManager;
 };
 
-#endif //IMAGEEDITOR_H
+#endif // IMAGEEDITOR_H
