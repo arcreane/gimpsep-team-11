1108aaeec05353a56f8aa3ce9a5ee84fd70b8561
김부경 <emilly03141221@gmail.com>
2024-06-09 21:34:16 +0200

    Makefile

---
+++ src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/main.cpp.obj"
+  "CMakeFiles/src.dir/main.cpp.obj.d"
+  "CMakeFiles/src.dir/src/Image.cpp.obj"
+  "CMakeFiles/src.dir/src/Image.cpp.obj.d"
+  "CMakeFiles/src.dir/src/ImageEditor.cpp.obj"
+  "CMakeFiles/src.dir/src/ImageEditor.cpp.obj.d"
+  "CMakeFiles/src.dir/src/ImageManager.cpp.obj"
+  "CMakeFiles/src.dir/src/ImageManager.cpp.obj.d"
+  "CMakeFiles/src.dir/src/Menu.cpp.obj"
+  "CMakeFiles/src.dir/src/Menu.cpp.obj.d"
+  "libsrc.dll.a"
+  "src.exe"
+  "src.exe.manifest"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
